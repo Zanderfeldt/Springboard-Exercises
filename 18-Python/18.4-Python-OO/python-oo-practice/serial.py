0f41c667a0252789a117497534ca50885b3b88f0
Alex <xela419@gmail.com>
2022-12-15 17:26:16 -0800

    fixed serial generator

--- 18-Python/18.4-Python-OO/python-oo-practice/serial.py
+++ 18-Python/18.4-Python-OO/python-oo-practice/serial.py
@@ -1,8 +1,9 @@
 """Python serial number generator."""
 
+
 class SerialGenerator:
     """Machine to create unique incrementing serial numbers.
-    
+
     >>> serial = SerialGenerator(start=100)
 
     >>> serial.generate()
@@ -20,3 +21,23 @@ class SerialGenerator:
     100
     """
 
+    def __init__(self, start=0):
+        """Make new generator, starting at start"""
+
+        self.start = self.next = start
+
+    def __repr__(self):
+        """Show representation"""
+
+        return f"<SerialGenerator start={self.start} next={self.next}>"
+
+    def generate(self):
+        """Return next serial"""
+
+        self.next += 1
+        return self.next - 1
+
+    def reset(self):
+        """Reset number to start"""
+
+        self.next = self.start
