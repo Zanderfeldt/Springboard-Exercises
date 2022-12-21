a29c9dae76ad8189c0138e194f3126bb31d0680b
Alex Vasquez <glooombook@Alexs-MacBook-Pro-2.local>
2022-12-21 13:34:37 -0800

    MadLib exercise completed

---
+++ 19-Flask/19.2-Flask-Jinja/flask-madlibs-exercise/stories.py
@@ -0,0 +1,56 @@
+"""Madlibs Stories."""
+
+
+class Story:
+    """Madlibs story.
+
+    To  make a story, pass a list of prompts, and the text
+    of the template.
+
+        >>> s = Story(["noun", "verb"],
+        ...     "I love to {verb} a good {noun}.")
+
+    To generate text from a story, pass in a dictionary-like thing
+    of {prompt: answer, promp:answer):
+
+        >>> ans = {"verb": "eat", "noun": "mango"}
+        >>> s.generate(ans)
+        'I love to eat a good mango.'
+    """
+
+    def __init__(self, code, title, words, text):
+        """Create story with words and template text."""
+
+        self.prompts = words
+        self.template = text
+        self.title = title
+        self.code = code
+
+    def generate(self, answers):
+        """Substitute answers into text."""
+
+        text = self.template
+
+        for (key, val) in answers.items():
+            text = text.replace("{" + key + "}", val)
+
+        return text
+
+
+# Here's a story to get you started
+
+
+story1 = Story('history', 'A History Tale',
+    ["place", "noun", "verb", "adjective", "plural_noun"],
+    """Once upon a time in a long-ago {place}, there lived a
+       large {adjective} {noun}. It loved to {verb} {plural_noun}."""
+)
+
+story2 = Story(
+    "omg",
+    "An Excited Adventure",
+    ["noun", "verb"],
+    """OMG!! OMG!! I love to {verb} a {noun}!"""
+)
+
+stories = {s.code: s for s in [story1, story2]}
